56dc31d9678ebacb9f0b136692c6ae848294808a
Damian Guth <guth-d@web.de>
2023-03-19 11:16:53 +0100

    Print some text to show activity.

--- UnityCSProjWatcher/Program.cs
+++ UnityCSProjWatcher/Program.cs
@@ -2,14 +2,19 @@
 
 string? command;
 
+Console.WriteLine("Registering watcher");
 Watcher watcher = new();
 watcher.WaitForFileChanges();
+Console.WriteLine("Waiting for filechanges");
+
+Console.WriteLine("type 'exit' to exit the application.");
 
 while(true)
 {
     command = Console.ReadLine();
     if(command == "exit")
     {
+        Console.WriteLine("Exiting...");
         Environment.Exit(0);
     }
 }
